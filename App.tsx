11a436eff65202951fa78cc463d8079ba45c8a85
Rehan7A <rehanansari6565@gmail.com>
2024-08-24 18:04:27 +0530

    added commit

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function App() {
+  console.log("App")
   return (
     <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
       <Text>App</Text>
